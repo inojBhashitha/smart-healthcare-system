9c62102e7987a2e21ebfeb3001a867d5f8c72e9b
inojBhashitha <main.inoj@gmail.com>
2026-06-29 20:34:15 +0530

    Implement POST endpoint for prescription creation using DTO

--- src/main/java/com/smarthealthcare/backend/controller/PrescriptionController.java
+++ src/main/java/com/smarthealthcare/backend/controller/PrescriptionController.java
@@ -4,6 +4,9 @@ import com.smarthealthcare.backend.entity.Prescription;
 import com.smarthealthcare.backend.service.PrescriptionService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import com.smarthealthcare.backend.dto.PrescriptionRequest;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 import java.util.List;
 
@@ -20,5 +23,13 @@ public class PrescriptionController {
     public List<Prescription> getPrescriptions() {
         return prescriptionService.getAllPrescriptions();
     }
+
+    @PostMapping("/api/prescriptions")
+    public Prescription createPrescription(
+        @RequestBody PrescriptionRequest request) {
+
+        return prescriptionService.createPrescription(request);
+
+}
 
 }
--- src/main/java/com/smarthealthcare/backend/service/PrescriptionService.java
+++ src/main/java/com/smarthealthcare/backend/service/PrescriptionService.java
@@ -3,6 +3,8 @@ package com.smarthealthcare.backend.service;
 import com.smarthealthcare.backend.entity.Prescription;
 import com.smarthealthcare.backend.repository.PrescriptionRepository;
 import org.springframework.stereotype.Service;
+import com.smarthealthcare.backend.dto.PrescriptionRequest;
+import java.time.LocalDateTime;
 
 import java.util.List;
 
@@ -18,5 +20,20 @@ public class PrescriptionService {
     public List<Prescription> getAllPrescriptions() {
         return prescriptionRepository.findAll();
     }
+
+    public Prescription createPrescription(PrescriptionRequest request) {
+
+    Prescription prescription = new Prescription();
+
+    prescription.setPatientId(request.getPatientId());
+    prescription.setImageUrl(request.getImageUrl());
+    prescription.setDoctorName(request.getDoctorName());
+    prescription.setHospital(request.getHospital());
+    prescription.setStatus(request.getStatus());
+
+    prescription.setScanDate(LocalDateTime.now());
+
+    return prescriptionRepository.save(prescription);
+}
 
 }
